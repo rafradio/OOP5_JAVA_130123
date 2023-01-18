32ca736df49db0a868988687fe2c87f66f5aa0e2
Rafail Abdyushev <rafaelab@mail.ru>
2023-01-18 12:15:46 +0300

    mvp modification

--- HomeWork_MVP/AttendentView/BadStudents.java
+++ HomeWork_MVP/AttendentView/BadStudents.java
@@ -10,35 +10,16 @@ public class BadStudents<T extends User> extends StatisticsView<T> {
         super(students);
     }
 
-    // @Override
-    // public void takeCriteria() {
-    //     for (T student : this.students) {
-    //         this.findCriteria(student);
-    //     }
-        
-        
-    // }
-
     @Override
     public void printReport() {
         for (T student : this.students) {
             if (((Student) student).criteria < 0.25f) {
                 System.out.println(student);
             }
-            // System.out.println(student);
+
         }
         
     }
 
-    // public void findCriteria(T student) {
-    //     int counter = 0;
-    //     for (Date i : student.getAttendense().keySet()) {
-    //         if (student.getAttendense().get(i)) counter++;
-            
-    //     }
-        
-    //     ((Student) student).criteria = (float) counter / student.getAttendense().size();
-    // }
-    
     
 }
